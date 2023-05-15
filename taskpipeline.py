3f9eb7b682326df40265956400b181e4f12c4169
Salmah Lasisi <44398948+salmah52@users.noreply.github.com>
2023-05-15 12:47:16 +0100

    A typical task pipeline block looks like this

---
+++ taskpipeline.py
@@ -0,0 +1,9 @@
+
+
+# task pipeline
+extract >> transform >> load
+
+
+#Task pipeline helps us to organize the order of tasks.
+
+#Here the task extract must run first, followed by transform, followed by the task load.
