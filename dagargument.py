aa771bb0a65239f3c7386ef08e32d77cc9cdb2c1
Salmah Lasisi <44398948+salmah52@users.noreply.github.com>
2023-05-15 12:28:42 +0100

    A typical DAG Arguments block looks like this

--- dagargument.py
+++ dagargument.py
@@ -10,3 +10,17 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+
+
+
+#DAG arguments are like settings for the DAG.
+
+#he above settings mention
+
+#the owner name,
+#when this DAG should run from: days_age(0) means today,
+#the email address where the alerts are sent to,
+#whether alert must be sent on failure,
+#whether alert must be sent on retry,
+#the number of retries in case of failure, and
+t#he time delay between retries.
